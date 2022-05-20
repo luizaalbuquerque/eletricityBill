701591627a653f82bd9da762e013616b1896da94
Gabriel <gabriel.pa16@gmail.com>
2022-05-20 20:55:01 +0100

    setting up test environment

---
+++ test/search.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const lunr = require('lunr');
+const search = require('../search');
+
+describe('search', () => {
+    describe('#find',() => {
+        it('should return results when a file matches a term');
+
+    });
+
+
+});
